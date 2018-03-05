612a5fc82c6487f28b0f22aabecf9b027058d953
jeroentvb <jeroen.van.berkum@hva.nl>
2018-03-05 21:58:28 +0100

    Added favicon and a subject in index

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var moreButton = document.getElementById("moreButton");
-var moreParagraph = document.getElementById("moreParagraph");
+var moreButton = document.getElementById('moreButton');
+var moreParagraph = document.getElementById('moreParagraph');
 
 function showMoreLess() {
-  if (moreParagraph.classList.contains("hideParagraph")) {
-    moreParagraph.classList.replace("hideParagraph", "showParagraph");
+  if (moreParagraph.classList.contains('hideParagraph')) {
+    moreParagraph.classList.replace('hideParagraph', 'showParagraph');
   } else {
-    moreParagraph.classList.replace("showParagraph", "hideParagraph");
+    moreParagraph.classList.replace('showParagraph', 'hideParagraph');
   }
 }
 
-moreButton.addEventListener("click", showMoreLess);
+moreButton.addEventListener('click', showMoreLess);
